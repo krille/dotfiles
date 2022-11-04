4b9a03479e17d17ffd13732dee8c4d40c6d1a61e
Kristofer Westling <kristofer@polr.se>
2022-11-04 08:21:25 +0100

    Added autocomplete

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,28 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.j
 
 Import-Module -Name Terminal-Icons
 
+#Imports PSReadLine
+Import-Module PSReadLine
+
+#Tab - Gives a menu of suggestions
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+#UpArrow will show the most recent command
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
+#DownArrow will show the least recent command
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+#During auto completion, pressing arrow key up or down will move the cursor to the end of the completion
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+#Shows tooltip during completion
+Set-PSReadLineOption -ShowToolTips
+
+#Gives completions/suggestions from historical commands
+Set-PSReadLineOption -PredictionSource History
+
+#Aliases
 New-Alias -Name c -Value Clear-Host
 
 function Get-Projects { & Set-Location C:\Source\Repos }
